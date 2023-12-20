6e067fbf3a8c7d9705baca057841cde3a3b6db1c
iaio998 <depalatisiacopo@gmail.com>
2023-12-20 16:08:35 +0100

    Added class on cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,10 +3,29 @@
 @section('title', 'Movies')
 
 @section('content')
-<h1>Movies</h1>
-<ul>
-    @foreach($movies as $movie)
-    <li><a href="{{route('movies.show', $movie->id)}}">{{$movie->title}}</a></li>
-    @endforeach
-</ul>
+
+<main id="main-content">
+    <div class="container bg-dark">
+        <h1>Movies</h1>
+        <div class="row gy-3">
+            @foreach($movies as $movie)
+            <div class="col-12 col-md-3 col-lg-4">
+                <div class="my-card">
+                    <div class="my-img-cont">
+                        <img src="{{$movie->image}}" alt="{{$movie->title}}">
+                    </div>
+                    <div class="my-text-cont text-center">
+                        <p>Title: {{$movie -> title}}</p>
+                        <p>Original language: {{$movie -> language}}</p>
+                        <p>Vote: {{$movie -> vote}}/10</p>
+                    </div>
+                    <div class="my-btn">
+                        <button><a href="{{route('movies.show', $movie->id)}}">SHOW MORE</a></button>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</main>
 @endsection
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,6 +3,24 @@
 @section('title', $movie->title)
 
 @section('content')
-<h2>Selected movie</h2>
-<h1>{{$movie->title}}</h1>
+<main id="main-content">
+    <div class="container bg-dark">
+        <h1>{{$movie -> title}}</h1>
+        <div class="row ">
+            <div class="col-12 col-md-3 col-lg-4">
+                <div class="my-card">
+                    <div class="my-img-cont">
+                        <img src="{{$movie->image}}" alt="{{$movie->title}}">
+                    </div>
+                    <div class="my-text-cont">
+                        <p>Title: {{$movie -> title}}</p>
+                        <p>Original language: {{$movie -> language}}</p>
+                        <p>Vote: {{$movie -> vote}}/10</p>
+                    </div>
+                    <button><a href="{{route('movies.index')}}">GO BACK</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
